31fd3e8d08ef636499f9ee157a4a339f32ef4fd3
Mehdi Karim <contact@mehdikarim.com>
2024-10-01 21:28:32 +0700

    Refactor page component and add LearnMoreFormDialog component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,61 @@
-import Image from "next/image";
+import { LearnMoreFormDialog } from "@/components";
+import Link from "next/link";
 
-export default function Home() {
+const Home: React.FC<{
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}> = async ({ searchParams }) => {
+  const sP = await searchParams;
+  const { form } = sP;
+
+  // type searchParamsType = string[][] | Record<string, string> | string | URLSearchParams;
+  console.log("sP", sP);
+
+  const getDialogToogleLink = () => {
+    const params = new URLSearchParams(
+      Object.entries(sP).map(([key, value]) => [
+        key,
+        Array.isArray(value) ? value.join(",") : value || "",
+      ]),
+    );
+
+    if (!form) {
+      params.append("form", "learn-more");
+      params.append("step", "1");
+      return `?${params.toString()}`;
+    }
+
+    params.delete("form");
+    params.delete("step");
+    return `?${params.toString()}`;
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className="flex flex-col items-center">
+      <header className="flex h-screen w-full flex-col items-center justify-center gap-4 px-4 sm:max-w-lg">
+        <hgroup className="flex flex-col items-center justify-center gap-4 px-4">
+          <p className="rounded-full border px-4 py-2 text-center">
+            Revolution in cancer treatment
+          </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          <h1 className="text-center text-3xl font-bold tracking-tight">
+            Nanoparticle-based Radiopharmaceutical
+          </h1>
+          <p className="text-center">
+            We develop the world first universal nanoparticle platform that
+            embeds radioactive materials to deliver targeted radiation to cancer
+            cells.
+          </p>
+        </hgroup>
+        <Link
+          href={getDialogToogleLink()}
+          className="mt-8 flex h-20 w-full items-center justify-center rounded-full bg-amber-500 text-lg font-bold dark:bg-amber-700"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          Learn More
+        </Link>
+      </header>
+      <LearnMoreFormDialog />
+    </main>
   );
-}
+};
+
+export default Home;
---
+++ src/components/learn-more-form-dialog.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import {
+  Description,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/react";
+import { useSearchParams } from "next/navigation";
+import { useMemo } from "react";
+
+const LearnMoreFormDialog: React.FC = () => {
+  const searchParam = useSearchParams();
+  const isOpen = useMemo(() => {
+    return searchParam.get("form") === "learn-more";
+  }, [searchParam]);
+
+  return (
+    <>
+      <button onClick={() => setIsOpen(true)}>Open dialog</button>
+      <Dialog
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        className="relative z-50"
+      >
+        <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+          <DialogPanel className="max-w-lg space-y-4 border bg-white p-12">
+            <DialogTitle className="font-bold">Deactivate account</DialogTitle>
+            <Description>
+              This will permanently deactivate your account
+            </Description>
+            <p>
+              Are you sure you want to deactivate your account? All of your data
+              will be permanently removed.
+            </p>
+            <div className="flex gap-4">
+              <button onClick={() => setIsOpen(false)}>Cancel</button>
+              <button onClick={() => setIsOpen(false)}>Deactivate</button>
+            </div>
+          </DialogPanel>
+        </div>
+      </Dialog>
+    </>
+  );
+};
+
+export { LearnMoreFormDialog };
