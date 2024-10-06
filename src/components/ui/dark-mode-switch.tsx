64a81d0b8c67dcdd9b614046a3c491990de17358
Mehdi Karim <contact@mehdikarim.com>
2024-10-07 00:49:08 +0700

    improve DarmModeSwitch style

--- src/components/ui/dark-mode-switch.tsx
+++ src/components/ui/dark-mode-switch.tsx
@@ -4,6 +4,7 @@ import dynamic from "next/dynamic";
 // import { Switch } from "@headlessui/react";
 import { useTheme } from "next-themes";
 import { LoaderCircle, Moon, Sun } from "lucide-react";
+import { cn } from "@/utils/cn";
 
 const Switch = dynamic(
   () => import("@headlessui/react").then((mod) => mod.Switch),
@@ -25,9 +26,6 @@ const DarkModeSwitch = () => {
     setTheme(isChecked ? "dark" : "light");
   };
 
-  // console.log("theme", theme);
-  // console.log("!!resolvedTheme", !!resolvedTheme);
-  // console.log("resolvedTheme", resolvedTheme);
   return (
     <Switch
       checked={
@@ -37,15 +35,25 @@ const DarkModeSwitch = () => {
           resolvedTheme === "dark")
       }
       onChange={handleChange}
-      className="group inline-flex h-6 w-11 items-center rounded-full bg-neutral-950 transition data-[checked]:bg-neutral-50"
+      className={cn(
+        "group inline-flex h-6 w-11 items-center rounded-full border transition",
+        "border-neutral-600 bg-neutral-200 text-neutral-300",
+        "dark:border-neutral-300 dark:bg-neutral-600 dark:text-neutral-600",
+      )}
     >
-      <div className="relative size-4 translate-x-1 rounded-full border-neutral-50 text-neutral-50 shadow  shadow-neutral-50 dark:shadow-neutral-950/50 transition group-data-[checked]:translate-x-6 dark:border-neutral-950 dark:text-neutral-950">
+      <div
+        className={cn(
+          "relative size-4 translate-x-1 rounded-full shadow-lg transition duration-500 ease-out dark:translate-x-6",
+          "bg-neutral-600",
+          "dark:bg-neutral-300",
+        )}
+      >
         <Sun
-          className="absolute left-1/2 top-1/2 z-10 size-2 -translate-x-1/2 -translate-y-1/2 opacity-100 group-data-[checked]:opacity-0"
+          className="absolute left-1/2 top-1/2 z-10 size-3 -translate-x-1/2 -translate-y-1/2 opacity-100 dark:opacity-0"
           fill="currentColor"
         />
         <Moon
-          className="absolute left-1/2 top-1/2 z-10 size-2 -translate-x-1/2 -translate-y-1/2 opacity-0 group-data-[checked]:opacity-100"
+          className="absolute left-1/2 top-1/2 z-10 size-3 -translate-x-1/2 -translate-y-1/2 opacity-0 dark:opacity-100"
           fill="currentColor"
         />
       </div>
